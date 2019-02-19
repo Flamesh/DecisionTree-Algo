dc7afae57b70b1b8a32b24c0d71edc0fbb5a6464
Dang Tuyen <32127615+Flamesh@users.noreply.github.com>
2019-02-19 16:02:30 +0700

    Update CART.py

--- CART.py
+++ CART.py
@@ -1,13 +1,10 @@
 from __future__ import print_function 
 import numpy as np 
 import pandas as pd 
-from sklearn.model_selection import train_test_split
-import pydotplus
-import time
+
 class TreeNode(object):
-    def __init__(self, ids = None, children = [], gini = 0, depth = 0):
+    def __init__(self, ids = None, children = [], depth = 0):
         self.ids = ids           # index of data in this node
-        self.gini = gini          # gini, will fill later
         self.depth = depth       # distance to root node
         self.split_attribute = None # which attribute is chosen, it non-leaf
         self.children = children # list of its child nodes
@@ -45,7 +42,7 @@ class DecisionTreeCART(object):
         self.labels = target.unique()
         
         ids = range(self.Ntrain)
-        self.root = TreeNode(ids = ids, gini = self._gini(ids), depth = 0)
+        self.root = TreeNode(ids = ids, depth = 0)
         queue = [self.root]
         while queue:
             node = queue.pop()
@@ -59,21 +56,18 @@ class DecisionTreeCART(object):
         
     def _gini(self, ids):
         if len(ids) == 0: return 0
-        ids = [i+1 for i in ids] #
+        ids = [i+1 for i in ids] 
         freq = np.array(self.target[ids].value_counts())     
-        #freq = np.array(self.target[ids].value_counts())
         return gini(freq)
 
 
     def _set_label(self, node):
-        # find label for a node if it is a leaf
-        # simply chose by major voting 
-        target_ids = [i + 1 for i in node.ids]  # target is a series variable
-        node.set_label(self.target[target_ids].mode()[0]) # most frequent label
+        
+        target_ids = [i + 1 for i in node.ids]  
+        node.set_label(self.target[target_ids].mode()[0]) 
     
     def _split(self, node):
         ids = node.ids 
-        #print(ids)
         best_gini = 0
         best_splits = []
         best_attribute = None
@@ -81,32 +75,30 @@ class DecisionTreeCART(object):
         sub_data = self.data.iloc[ids, :]
         for i, att in enumerate(self.attributes):
             values = self.data.iloc[ids, i].unique().tolist()
-            #print(values)
-            if len(values) == 1: continue # entropy = 0
+            
+            if len(values) == 1: continue 
             splits = []
-            #print(values)
             for val in values: 
                 sub_ids = sub_data.index[sub_data[att] == val].tolist()
                 splits.append([sub_id-1 for sub_id in sub_ids])
             
             # don't split if a node has too small number of points
             if min(map(len, splits)) < self.min_samples_split: continue
-            # information gain
-            HxS = 0.0
+            
+            Gini = 0.0
             #print(type(HxS))
             for split in splits:
-                #HxS += len(split)*self._entropy(split)/len(ids) 
-                HxS += len(split)*self._gini(split)/len(ids)
-            #print(gini)
-            if HxS < self.min_gini: continue # stop if small gain 
-            if HxS > best_gini:
-                best_gini = HxS
+                Gini += len(split)*self._gini(split)/len(ids)
+            
+            if Gini < self.min_gini: continue # stop if small gain 
+            if Gini > best_gini:
+                best_gini = Gini
                 best_splits = splits
                 best_attribute = att
                 order = values
         node.set_properties(best_attribute, order)
         child_nodes = [TreeNode(ids = split,
-                      gini = self._gini(split), depth = node.depth + 1) for split in best_splits]
+                       depth = node.depth + 1) for split in best_splits]
         return child_nodes
 
     def predict(self, new_data):
@@ -115,12 +107,10 @@ class DecisionTreeCART(object):
         
         labels = [None]*npoints
         for n in range(npoints):
-            x = new_data.iloc[n, :] # one point 
-            # start from root and recursively travel if not meet a leaf 
+            x = new_data.iloc[n, :]
             node = self.root
             while node.children: 
                 node = node.children[node.order.index(x[node.split_attribute])]
             labels[n] = node.label
         
         return labels
-
